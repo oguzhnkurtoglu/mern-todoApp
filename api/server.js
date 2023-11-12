cb7df98ea46ef51a0e9156d1d063fc618f284c0c
= <=>
2023-11-12 21:04:23 +0300

    crud işlemleri eklendi

--- api/server.js
+++ api/server.js
@@ -39,14 +39,44 @@ app.post("/todos/new", async (req, res) => {
 	}
 })
 app.delete("/todos/delete/:id", async (req, res) => {
-	const result = new Todo.findById(req.params.id)
-	res.json(result)
+	try {
+		// Belirli bir id'ye sahip Todo öğesini bul ve sil
+		const result = await Todo.findByIdAndDelete(req.params.id)
+
+		// Todo bulunamazsa hata döndür
+		if (!result) {
+			return res.status(404).json({ error: "Todo bulunamadı" })
+		}
+
+		// Silinen todo'nun bilgisini JSON olarak döndür
+		res.json({ message: "Todo başarıyla silindi", deletedTodo: result })
+	} catch (error) {
+		// Hata durumunda hata mesajını JSON olarak döndür
+		res.status(500).json({ error: error.message })
+	}
 })
 app.put("/todos/complete/:id", async (req, res) => {
-	const todo = new Todo.findById(req.params.id)
-	todo.complete = !todo.complete
-	todo.save()
-	res.json(todo)
+	try {
+		// Belirli bir id'ye sahip Todo öğesini bul
+		const todo = await Todo.findById(req.params.id)
+
+		// Todo bulunamazsa hata döndür
+		if (!todo) {
+			return res.status(404).json({ error: "Todo bulunamadı" })
+		}
+
+		// complete alanını tersine çevir
+		todo.complete = !todo.complete
+
+		// Değişiklikleri kaydet
+		await todo.save()
+
+		// Güncellenmiş todo'yu JSON olarak döndür
+		res.json(todo)
+	} catch (error) {
+		// Hata durumunda hata mesajını JSON olarak döndür
+		res.status(500).json({ error: error.message })
+	}
 })
 
 app.listen(3001, () => console.log("Server Started 3001"))
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,83 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { useRef } from "react"
 
 function App() {
 	const [todos, setTodos] = useState([])
 	const [newTodo, setnewTodo] = useState("")
-	const [popupActive, setpopupActive] = useState([false])
+	// const [popupActive, setpopupActive] = useState([false])
+	const myRef = useRef("")
+
+	const getData = () => {
+		axios.get("http://localhost:3001/todos").then(data => setTodos(data.data))
+	}
+	const completeTodos = id => {
+		axios.put(`http://localhost:3001/todos/complete/${id}`)
+	}
+	const deleteTodos = id => {
+		axios.delete(`http://localhost:3001/todos/delete/${id}`)
+	}
+	const addTodos = async newTodo => {
+		try {
+			// Axios ile POST isteği gönder
+			const response = await axios.post(
+				"http://localhost:3001/todos/new",
+				newTodo.json
+			)
+			console.log("Başarıyla gönderildi:", response.data)
+		} catch (error) {
+			console.error("Gönderme hatası:", error.message)
+		}
+	}
 
 	useEffect(() => {
-		axios.get("http://localhost:3001/todos").then(data => setTodos(data.data))
+		getData()
 	}, [])
 
 	return (
-		<>
+		<div>
 			<h1>Wellcome ,Oguzhan</h1>
-			<h4>Your Taks</h4>
-
+			<h4>Your Tasks</h4>
+			<div className="addTask">
+				<input
+					ref={myRef}
+					placeholder="Add a new task"
+					type="text"
+					onKeyDown={e => {
+						if (e.key === "Enter") {
+							setnewTodo(myRef.current.value)
+							myRef.current.value = ""
+							addTodos(newTodo)
+						}
+					}}
+				/>
+			</div>
 			<div className="Todos">
 				{todos.map(todos => (
-					<div className="Todo">
+					<div key={todos._id} className="Todo">
 						<div className="checkbox"></div>
-						<div className="text">{todos.todo}</div>
-						<div className="delete-todo">x</div>
+						<div
+							onClick={() => {
+								completeTodos(todos._id)
+							}}
+							className="text"
+						>
+							{todos.todo}
+						</div>
+						<div className="Completed">{todos.complete ? "1" : "2"}</div>
+						<div className="date">{todos.date}</div>
+						<div
+							className="delete"
+							onClick={() => {
+								deleteTodos(todos._id)
+							}}
+						>
+							X
+						</div>
 					</div>
 				))}
 			</div>
-		</>
+		</div>
 	)
 }
 
