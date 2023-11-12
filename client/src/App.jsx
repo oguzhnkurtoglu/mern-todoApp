79c925d0cae0584488b9e0c01a2786171348e774
= <=>
2023-11-12 05:34:48 +0300

    dbden data gelmeme sorunu cozuldu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,15 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-const API_BASE_URL = "https://localhost/3001"
-
 function App() {
 	const [todos, setTodos] = useState([])
 	const [newTodo, setnewTodo] = useState("")
 	const [popupActive, setpopupActive] = useState([false])
 
 	useEffect(() => {
-		getTodos(API_BASE_URL + "/todos")
-		console.log(todos)
+		axios.get("http://localhost:3001/todos").then(data => setTodos(data.data))
 	}, [])
 
-	const getTodos = url => {
-		axios
-			.get(url)
-			.then(response => setTodos(response))
-			.catch(error => console.log(error, "Error"))
-	}
 	return (
 		<>
 			<h1>Wellcome ,Oguzhan</h1>
@@ -26,9 +17,9 @@ function App() {
 
 			<div className="Todos">
 				{todos.map(todos => (
-					<div key={todos.id} className="Todo">
+					<div className="Todo">
 						<div className="checkbox"></div>
-						<div className="text">Buy Bread</div>
+						<div className="text">{todos.todo}</div>
 						<div className="delete-todo">x</div>
 					</div>
 				))}
